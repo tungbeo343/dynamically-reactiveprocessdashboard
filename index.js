f553bc1e1c44a92003baf0b31e338970aee1a515
tungbeo343 <jonahreecepun0suy@hotmail.com>
2024-05-09 15:09:38 +0700

    update sdk task commit

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-const uniqueElements = (arr) => [...new Set(arr)];
+function rotate(matrix) {
+  const n = matrix.length;
+  for (let i = 0; i < Math.floor(n / 2); i++) {
+    for (let j = i; j < n - i - 1; j++) {
+      const temp = matrix[i][j];
+      matrix[i][j] = matrix[n - j - 1][i];
+      matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
+      matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
+      matrix[j][n - i - 1] = temp;
+    }
+  }
+}
